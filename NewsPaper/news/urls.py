2f1c76a15b1022f8315c3a7780f140b740f85112
AleksandrMK <jasonc@mail.ru>
2023-03-02 18:18:38 +0400

    Create new views

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-
-from .views import PostList, PostDetail
+from .views import NewsList, NewsDetail, NewsSearch, PostDelete, ArticlesList, PostList, \
+   ArticlesDetail, NewsEdit, ArticlesEdit, ArticlesCreate, NewsCreate, news
 
 urlpatterns = [
-    path('', PostList.as_view()),
-    path('<int:pk>', PostDetail.as_view())
+   path('', PostList.as_view(), name='post_list'),
+   path('create/', news, name='create'),
+   path('search/', NewsSearch.as_view(), name='search'),
+   path('news/', NewsList.as_view(), name='news_list'),
+   path('articles/', ArticlesList.as_view(), name='articles_list'),
+   path('news/<int:pk>', NewsDetail.as_view(), name='one_news'),
+   path('articles/<int:pk>', ArticlesDetail.as_view(), name='one_articles'),
+   path('articles/create/', ArticlesCreate.as_view(), name='articles_create'),
+   path('news/create/', NewsCreate.as_view(), name='news_create'),
+   path('articles/<int:pk>/edit/', ArticlesEdit.as_view(), name='news_edit'),
+   path('news/<int:pk>/edit/', NewsEdit.as_view(), name='articles_edit'),
+   path('articles/<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
+   path('news/<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
 ]
