fee49f66dbe2e45e9454f902f23eef4e030da21e
AleksandrMK <jasonc@mail.ru>
2023-03-03 18:15:22 +0400

    Project test

---
+++ project/simpleapp/views.py
@@ -0,0 +1,54 @@
+# Импортируем класс, который говорит нам о том,
+# что в этом представлении мы будем выводить список объектов из БД
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
+from .models import Product
+from .filters import ProductFilter
+from .forms import ProductForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    template_name = 'products.html'
+    context_object_name = 'products'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class ProductDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Product
+    # Используем другой шаблон — product.html
+    template_name = 'product.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'product'
+
+
+class ProductCreate(LoginRequiredMixin, CreateView):
+    raise_exception = True
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+
+class ProductUpdate(UpdateView):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('product_list')
